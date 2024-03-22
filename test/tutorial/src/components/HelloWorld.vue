b53a6a4d9dd0c0a4462c2a44c9dccb67a4fabd74
porwai <porwaiwit@gmail.com>
2024-03-22 15:58:35 -0400

    initial

---
+++ test/tutorial/src/components/HelloWorld.vue
@@ -0,0 +1,94 @@
+<script setup>
+
+console.log("hello princeton")
+
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0);
+
+const name = ref(0);
+
+const incrementCount = () => {
+
+  console.log("click")
+  fetch(`/increment/${count.value}`)
+    .then((response) => {
+      if (response.ok) {
+        return response.text();
+      } else {
+        throw new Error(`${response.status} ${response.statusText}`);
+      }
+    })
+    .then((val) => {
+      count.value = parseInt(val);
+    })
+    .catch((error) => {
+      console.error(`Error: ${error.message}`);
+    });
+};
+
+
+const talker = () => {
+
+  console.log("talk flask")
+  fetch(`/talk/`)
+    .then((response) => {
+      if (response.ok) {
+        return response.text();
+      } else {
+        throw new Error(`${response.status} ${response.statusText}`);
+      }
+    })
+    .then((val) => {
+      name.value = val;
+    })
+    .catch((error) => {
+      console.error(`Error: ${error.message}`);
+    });
+
+  
+}
+
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+<div class="card">
+  <button type="button" @click="incrementCount">count asdf is {{ count }}</button>
+  <p>
+    Edit
+    <code>components/HelloWorld.vue</code> to test HMR
+  </p>
+</div>
+
+<div class = "talker" @click="talker">
+
+Hello new component here {{ name }}  Por
+</div>
+
+  <p>
+    Check out
+    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
+      >create-vue</a
+    >, the official Vue + Vite starter
+  </p>
+  <p>
+    Install
+    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
+    in your IDE for a better DX
+  </p>
+  <p class="read-the-docs">Click oasdfn the Vite and Vue logos to learn more</p>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
+
+
