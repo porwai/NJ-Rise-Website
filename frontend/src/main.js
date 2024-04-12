24f03b710167de6f35d8487c6c8bfa21c4f82ed6
porwai <porwaiwit@gmail.com>
2024-04-12 00:42:33 -0400

    Revert "shivam and jocelyn's finished login"
    This reverts commit bdb16b4f8985e9cb43886eb07e300641aa8154d2.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,28 +3,10 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.js'
 
 import { createApp } from 'vue'
-import { createStore } from 'vuex'
 import App from './App.vue'
 import router from './router'
 
-// Create a new store instance.
-const store = createStore({
-    state () {
-      return {
-        login_status: false
-      }
-    },
-    mutations: {
-      log_in (state) {
-        state.login_status = true
-      },
-      log_out (state) {
-        state.login_status = false
-      }
-    }
-  })
-
 const app = createApp(App)
 app.use(router)
-app.use(store)
+
 app.mount('#app')
