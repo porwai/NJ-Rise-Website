1f745a89799049e5b206d3f2556598b3c4f21479
Shivamkak19 <117538014+Shivamkak19@users.noreply.github.com>
2024-04-24 16:30:39 -0400

    logout button functionality operational

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,62 +1,153 @@
 <template>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
         <!-- Logo on the left -->
         <a class="navbar-brand" href="/">
-            <img src="/src/assets/RiseLogo.png" alt="NJ Rise logo" width="80">
+          <img src="/src/assets/RiseLogo.png" alt="NJ Rise logo" width="80">
         </a>
+  
+        <!-- Sliding toggle button -->
+        <div class="navbar-toggler-toggle" @click="toggleClick">
+          <transition name="toggle-transition">
+            <span v-if="togglePosition === 'left'" class="toggle-off">
+              <!-- SVG for toggle off state -->
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" width="60" height="60" class = "svgtoggle">
+                <!-- Your SVG path for toggle off state -->
+                <path d="M280-240q-100 0-170-70T40-480q0-100 70-170t170-70h400q100 0 170 70t70 170q0 100-70 170t-170 70H280Zm0-80h400q66 0 113-47t47-113q0-66-47-113t-113-47H280q-66 0-113 47t-47 113q0 66 47 113t113 47Zm0-40q50 0 85-35t35-85q0-50-35-85t-85-35q-50 0-85 35t-35 85q0 50 35 85t85 35Zm200-120Z" />
+              </svg>
+            </span>
+            <span v-else class="toggle-on">
+              <!-- SVG for toggle on state -->
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" width="60" height="60" class = "svgtoggle">
+                <!-- Your SVG path for toggle off state -->
 
-        <!-- Toggler button -->
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarMain" aria-controls="navbarMain" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+                <path d="M280-240q-100 0-170-70T40-480q0-100 70-170t170-70h400q100 0 170 70t70 170q0 100-70 170t-170 70H280Zm0-80h400q66 0 113-47t47-113q0-66-47-113t-113-47H280q-66 0-113 47t-47 113q0 66 47 113t113 47Zm400-40q50 0 85-35t35-85q0-50-35-85t-85-35q-50 0-85 35t-35 85q0 50 35 85t85 35ZM480-480Z" />
+              </svg>
+            </span>
+          </transition>
+
+          <!-- Text next to toggle button -->
+          <span class="toggle-text nav-link"> View: {{ toggleText }}</span>
+        </div>
+  
 
         <!-- Navigation links on the right -->
         <div class="collapse navbar-collapse justify-content-end" id="navbarMain">
-            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Pantry Dashboard
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="/addwalkin">
-                            <i class="fas fa-user-plus mr-1"></i>
-                            Add Walk In Client
-                        </a>
-                        <a class="dropdown-item" href="/registernewclient">
-                            <i class="fas fa-user-edit mr-1"></i>
-                            Register Client
-                        </a>
-                    </div>
-                </li>
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Pantry Dashboard
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="/addwalkin">
+                  <i class="fas fa-user-plus mr-1"></i>
+                  Add Walk In Client
+                </a>
+                <a class="dropdown-item" href="/registernewclient">
+                  <i class="fas fa-user-edit mr-1"></i>
+                  Register Client
+                </a>
+              </div>
+            </li>
+  
+            <li class="nav-item">
+              <a class="nav-link" href="/search">Client Search</a>
+            </li>
+  
+            <li class="nav-item">
+              <a class="nav-link" href="/reports">Reports</a>
+            </li>
+  
+            <li class="nav-item">
+              <span class="nav-link disabled">Staff</span> <!-- Changed to span for semantics -->
+            </li>
+  
+            <li v-if="this.$store.state.login_status === 'not_authorized'" class="nav-item">
+              <router-link class="nav-link" to="/login"> Login</router-link>
+            </li>
 
-                <li class="nav-item">
-                    <a class="nav-link" href="/search">Client Search</a>
-                </li>
-
-                <li class="nav-item">
-                    <a class="nav-link" href="/reports">Reports</a>
-                </li>
-
-                <li class="nav-item">
-                    <span class="nav-link disabled">Staff</span> <!-- Changed to span for semantics -->
-                </li>
-
-                <li class="nav-item">
-                        <router-link class="nav-link" to="/login"> Login</router-link>
-                </li>
-
-                <li class="nav-item">
-                        <router-link class="nav-link" to="/addnewuser"> Add New User </router-link>
-                </li>
-            </ul>
+            <li v-else class="nav-item">
+              <router-link class="nav-link" to="/login" @click="logoutClick"> Logout</router-link>
+            </li>
+  
+            <li class="nav-item">
+              <router-link class="nav-link" to="/addnewuser"> Add New User </router-link>
+            </li>
+          </ul>
         </div>
-    </div>
-</nav>
-</template>
+      </div>
+    </nav>
+  </template>
+  
+  <script>
+  export default {
+    name: 'NavBar',
+    data() {
+      return {
+        togglePosition: 'left', // Initial position of the toggle
+        toggleText: "Volunteer",
+        login_status: "admin"
+      };
+    },
+    methods: {
+      toggleClick() {
+        if (this.togglePosition === 'left') {
+          console.log('click right');
+          this.togglePosition = 'right';
+          this.toggleText = "Administrator";
+          this.$store.commit('log_in_admin')
+          console.log("login status", this.$store.state.login_status)
+          console.log("viewing status", this.$store.state.viewing_status)
 
-<script>
-export default {
-    name: 'NavBar'
-}
-</script>
+        } else {
+          console.log('click left');
+          this.togglePosition = 'left';
+          this.toggleText = "Volunteer";
+          this.$store.commit('log_in_admin_volunteer_view')
+          console.log("login status", this.$store.state.login_status)
+          console.log("viewing status", this.$store.state.viewing_status)
+
+
+        }
+      },
+      // function to logout user
+      logoutClick() {
+        console.log("BEFORE login status", this.$store.state.login_status)
+        console.log("viewing status", this.$store.state.viewing_status)
+        this.$store.commit('log_out')
+        console.log("login status", this.$store.state.login_status)
+        console.log("viewing status", this.$store.state.viewing_status)
+      }
+    }
+  }
+  </script>
+  
+  <style>
+  .navbar-toggler-toggle {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 60px; /* Increased width */
+    cursor: pointer;
+    margin-left: 300px; /* Shift to the right */
+  }
+  
+  .toggle-on svg, .toggle-off svg {
+    width: 80px; /* Larger size */
+    height: 80px; /* Larger size */
+    fill: #459bcd; /* Blue color */
+  }
+  
+  .toggle-off svg {
+    margin-right: 20px; /* Add margin for spacing */
+  }
+
+  .toggle-on svg {
+    margin-right: 20px; /* Add margin for spacing */
+  }
+  
+  .toggle-on svg:hover, .toggle-off svg:hover {
+    fill: #19527c; /* Darker blue on hover */
+  }
+  </style>
+  
--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,22 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import UserDetails from '../components/UserDetails.vue'
-import DataTable from '../components/DataTable.vue'
-import LoginView from '../views/LoginView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import UserDetails from '../components/UserDetails.vue';
+import DataTable from '../components/DataTable.vue';
+import LoginView from '../views/LoginView.vue';
+
+import store from '../main'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'login',
-      component: LoginView
+      name: 'home',
+      component: HomeView,
+      meta: {
+        requiresAuth: true // Example: Home page requires authentication
+      }
     },
     {
       path: '/about',
@@ -23,36 +29,66 @@ const router = createRouter({
     {
       path: '/userdetails',
       name: 'userdetails',
-      component: UserDetails
+      component: UserDetails,
+      meta: {
+        requiresAuth: true // Example: User details page requires authentication
+      }
     },
     {
       path: '/login',
       name: 'login',
-      // route level code-splitting
-      component: () => import('../views/LoginView.vue')
+      component: LoginView
     },
     {
       path: '/addnewuser',
       name: 'addnewuser',
-      // route level code-splitting
-      component: () => import('../views/AddNewUserView.vue')
+      component: () => import('../views/AddNewUserView.vue'),
+      meta: {
+        requiresAuth: true // Example: Add new user page requires authentication
+      }
     },
     {
       path: '/search',
       name: 'search',
-      component: () => import('../views/HomeView.vue')
+      component: HomeView,
+      meta: {
+        requiresAuth: true // Example: Search page requires authentication
+      }
     },
     {
       path: '/addwalkin',
       name: 'addwalkin',
-      component: () => import('../views/AddWalkIn.vue')
+      component: () => import('../views/AddWalkIn.vue'),
+      meta: {
+        requiresAuth: true // Example: Add walk-in page requires authentication
+      }
     }, 
     {
       path: '/registernewclient',
       name: 'registernewclient',
-      component: () => import('../views/RegisterClient.vue')
+      component: () => import('../views/RegisterClient.vue'),
+      meta: {
+        requiresAuth: true // Example: Register new client page requires authentication
+      }
     }
   ]
-})
+});
 
-export default router
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+  const isAuthenticated = store.state.login_status !== 'not_authorized';
+
+  console.log("in router:")
+  console.log("login status", store.state.login_status)
+  console.log("viewing status", store.state.viewing_status)
+
+  if (requiresAuth && !isAuthenticated) {
+    // Redirect to login page
+    next('/login');
+  } else {
+    // Proceed to the next middleware or route
+    next();
+  }
+});
+
+export default router;
