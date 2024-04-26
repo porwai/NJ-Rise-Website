6987d715e9c971cd0423a46350584eec6d47c0b9
Michael Huang <mhuang909@gmail.com>
2024-04-25 22:39:21 -0400

    fixed localhost api endpoint

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -56,7 +56,7 @@ export default {
   },
   methods: {
     async submitData() {
-      const response = await fetch('http://localhost:5000/api/login', {
+      const response = await fetch('/api/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
